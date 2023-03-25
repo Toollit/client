dd12a8f14772e97dd94d28530b30794c5824983e
seungwonleee <seungwon.code@gmail.com>
2023-03-25 23:30:09 +0900

    Add: image upload api
    - upload api to help authors preview in Tui web editor

---
+++ apis/project/uploadImage.ts
@@ -0,0 +1,22 @@
+import { serverInstance } from 'apis/axios';
+
+export type UploadImageAPIReq = FormData;
+
+export interface UploadImageAPIRes {
+  success: boolean;
+  message: string | null;
+  data: {
+    url: string;
+    alt: string;
+  };
+}
+
+export const uploadImageAPI = async (
+  image: UploadImageAPIReq,
+): Promise<UploadImageAPIRes | undefined> => {
+  const response = await serverInstance.post(
+    'post/project/content/uploadImage',
+    image,
+  );
+  return response.data;
+};
