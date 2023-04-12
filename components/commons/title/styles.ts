26816d055fe6cfb525bb8ca32da43f6a72a54c67
seungwonleee <seungwon.code@gmail.com>
2023-04-12 22:27:25 +0900

    Style: title componet style responsive

--- components/commons/title/styles.ts
+++ components/commons/title/styles.ts
@@ -1,15 +1,20 @@
+import { mediaQueryMobile } from '@/styles/mediaQuery';
 import styled from '@emotion/styled';
 
 const TitleContainer = styled.div`
-  height: 7.2rem;
-  margin-bottom: 2rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  min-height: 7.2rem;
+  height: fit-content;
+  padding: 2.5rem 0rem;
+  width: 100%;
+  word-wrap: break-word;
 `;
 
 const TitleText = styled.h1`
   font-size: ${(props) => props.theme.fontSizes.xxxlarge};
+
+  ${mediaQueryMobile} {
+    font-size: ${(props) => props.theme.fontSizes.large};
+  }
 `;
 
 export { TitleContainer, TitleText };
