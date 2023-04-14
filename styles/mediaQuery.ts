a82e3ef93e478215edd1348c4a89a78093d208e7
seungwonleee <seungwon.code@gmail.com>
2023-04-14 16:40:54 +0900

    Style: break point fix

--- styles/mediaQuery.ts
+++ styles/mediaQuery.ts
@@ -1,4 +1,13 @@
-const mediaQueryTablet = `@media (max-width: 768px)`;
-const mediaQueryMobile = `@media (max-width: 480px)`;
+// break point reference
+// Mobile S – 320px
+// Mobile M – 375px
+// Mobile L – 425px
+// Tablet – 768px
+// Laptop – 1024px
+// Laptop L – 1440px
+// 4K – 2560px
+
+const mediaQueryTablet = `@media (min-width: 768px)`;
+const mediaQueryMobile = `@media (min-width: 425px)`;
 
 export { mediaQueryTablet, mediaQueryMobile };
