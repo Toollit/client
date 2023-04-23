81bfc901fda34d059c84f93fe91d0e263236399f
seungwonleee <seungwon.code@gmail.com>
2023-04-23 20:06:05 +0900

    Style: input component
    - up and down spacing, match style

--- components/commons/input/index.tsx
+++ components/commons/input/index.tsx
@@ -8,6 +8,10 @@ interface InputProps {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+/**
+ * focus시 border color가 바뀌는 Input
+ * @param type - text 또는 password
+ */
 const Input = ({ type = 'text', value, placeholder, onChange }: InputProps) => {
   return (
     <StyledInput
--- components/commons/input/styles.ts
+++ components/commons/input/styles.ts
@@ -1,15 +1,16 @@
 import styled from '@emotion/styled';
 
 const StyledInput = styled.input`
-  border: 1px solid #cfd9de;
+  border: 1px solid ${(props) => props.theme.colors.border.base};
+  width: 100%;
   height: 5rem;
   border-radius: ${(props) => props.theme.borderRadius.sharp};
   padding: 0 1rem;
   font-size: 1.6rem;
-  margin-bottom: 2rem;
+  margin-top: 1.2rem;
+  margin-bottom: 1.2rem;
   z-index: 10;
 
-  /* TODO focus 속성 공통 속성으로 놔둘지 말지 수정하기 */
   &:focus {
     border-style: solid;
     border-color: ${(props) => props.theme.colors.theme};
