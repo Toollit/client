3233add03793ad850dbeebdee1796e31e108a8b0
seungwonleee <seungwon.code@gmail.com>
2023-04-30 18:03:24 +0900

    Fix: project handling more button (post remove, update)

--- components/commons/moreButton/index.tsx
+++ components/commons/moreButton/index.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import DeleteIcon from '@/assets/icons/DeleteIcon';
 import EditIcon from '@/assets/icons/EditIcon';
 import MoreIcon from '@/assets/icons/MoreIcon';
 import { Button, MoreMenu, Item, ItemContainer } from './styles';
 import { useDispatch } from 'react-redux';
 import { useRouter } from 'next/router';
+import { deletePostAPI } from '@/apis/deletePost';
+import { errorMessage } from '@/apis/errorMessage';
 
 interface MoreProps {
   isMine: boolean;
@@ -16,31 +18,11 @@ interface MoreProps {
 const More = ({ isMine }: MoreProps) => {
   const dispatch = useDispatch();
   const router = useRouter();
+  const postId = router.query.id;
 
+  const [postType, setPostType] = useState<'project' | 'free' | 'question'>();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-
-  const postInfo = useCallback(() => {
-    const boardType = router.asPath.split('/').find((str) => {
-      switch (str) {
-        case 'project':
-          return 'project';
-        case 'free':
-          return 'free';
-        case 'question':
-          return 'question';
-        default:
-          break;
-      }
-    });
-
-    const postId = router.query.id;
-
-    return {
-      boardType,
-      postId,
-    };
-  }, [router]);
 
   const handleClick = useCallback(
     (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -54,20 +36,53 @@ const More = ({ isMine }: MoreProps) => {
   }, []);
 
   const handleModify = useCallback(() => {
-    window.open(`/project/${postInfo().postId}/modify`);
+    window.open(`/modify/${postType}/${postId}`);
 
     setAnchorEl(null);
-  }, [postInfo]);
+  }, [postType, postId]);
 
-  const handleDelete = useCallback(() => {
-    console.log('handleDelete');
+  const handleDelete = useCallback(async () => {
     setAnchorEl(null);
-  }, []);
+
+    if (
+      postType !== undefined &&
+      (postType === 'project' || 'free' || 'question') &&
+      typeof postId === 'string'
+    ) {
+      const isDeletedOk = confirm('정말로 삭제하시겠습니까?');
+
+      if (isDeletedOk) {
+        try {
+          await deletePostAPI({ postType, postId });
+
+          router.replace('/');
+        } catch (error) {
+          errorMessage(error);
+        }
+      }
+    }
+  }, [postType, postId, router]);
 
   const handleReport = useCallback(() => {
     console.log('handleReport');
     setAnchorEl(null);
   }, []);
+
+  useEffect(() => {
+    const postType = router.asPath.split('/').find((type) => {
+      if (type === 'project') {
+        return setPostType('project');
+      }
+
+      if (type === 'free') {
+        return setPostType('free');
+      }
+
+      if (type === 'question') {
+        return setPostType('question');
+      }
+    });
+  }, [router]);
 
   return (
     <div>
