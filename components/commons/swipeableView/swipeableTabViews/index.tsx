e9de9b10eda5a287042998eedcbdd81faaf57a72
seungwonleee <seungwon.code@gmail.com>
2023-05-26 23:39:12 +0900

    WIP: swipeableTabViews

--- components/commons/swipeableView/swipeableTabViews/index.tsx
+++ components/commons/swipeableView/swipeableTabViews/index.tsx
@@ -1,47 +1,108 @@
-import React, { useRef, useState } from 'react';
-import SwipeableViews from 'react-swipeable-views';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import SwipeableViews, {
+  OnSwitchingCallbackTypeDescriptor,
+} from 'react-swipeable-views';
 import { CustomMuiTabs, CustomMuiTab } from './styles';
+import { useDispatch } from 'react-redux';
+import { updateSwipeableViewState } from '@/features/swipeableView';
 
 interface SwipeableTabViewProps {
-  tabs: string[];
+  tabs: { name: string; query: string }[];
   children: React.ReactNode;
+  // tabHandler: () => void;
 }
 
-const SwipeableTabView = ({ tabs, children }: SwipeableTabViewProps) => {
-  const [index, setIndex] = useState(0);
-  const targetRef = useRef<any>([]);
+const SwipeableTabView = ({
+  tabs,
+  children,
+}: // tabHandler,
+SwipeableTabViewProps) => {
+  const dispatch = useDispatch();
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, value: number) => {
-    setIndex(value);
-  };
+  const tabRefs = useRef<HTMLDivElement[]>([]);
 
-  const handleChangeIndex = (index: number) => {
-    setIndex(index);
-  };
+  const [indicatorPosition, setIndicatorPosition] = useState(0);
+
+  const handleChange = useCallback(
+    (event: React.SyntheticEvent, value: number) => {
+      setCurrentTabIndex(value);
+    },
+    [],
+  );
+
+  const handleChangeIndex = useCallback((index: number) => {
+    setCurrentTabIndex(index);
+  }, []);
+
+  const handleSwitching = useCallback(
+    (swipeDegree: number, type: OnSwitchingCallbackTypeDescriptor) => {
+      // swipe left
+      if (swipeDegree < currentTabIndex) {
+        const width: number = tabRefs.current[currentTabIndex - 1]?.offsetWidth;
+
+        const decimal = swipeDegree.toString().split('.')[1]?.slice(0, 2); // 소수점 이하 값만 가져옴
+
+        if (decimal === undefined) {
+          return;
+        }
+        const percent = Number(decimal);
+
+        setIndicatorPosition((100 - (width / 100) * percent) * -1);
+      }
+
+      // swipe right
+      if (swipeDegree > currentTabIndex) {
+        const width: number = tabRefs.current[currentTabIndex + 1]?.offsetWidth;
+
+        const decimal = swipeDegree.toString().split('.')[1]?.slice(0, 2); // 소수점 이하 값만 가져옴
+
+        if (decimal === undefined) {
+          return;
+        }
+        const percent = Number(decimal);
+
+        setIndicatorPosition((width / 100) * percent);
+      }
+
+      // if (type === 'end') {
+      //   console.log('currentTabIndex ===>', currentTabIndex);
+      //   setIndicatorPosition(tabRefs.current[currentTabIndex]?.offsetWidth);
+      // }
+    },
+    [currentTabIndex],
+  );
+
+  useEffect(() => {
+    dispatch(updateSwipeableViewState({ tabIndex: currentTabIndex }));
+  }, [currentTabIndex, dispatch]);
 
   return (
     <div>
       <CustomMuiTabs
-        value={index}
+        value={currentTabIndex}
         onChange={handleChange}
         disabled
-        width={targetRef.current[index]?.offsetWidth}
+        width={tabRefs.current[currentTabIndex]?.offsetWidth}
       >
-        {tabs.map((val, index) => {
+        {tabs.map((tab, index) => {
           return (
             <CustomMuiTab
-              key={val}
-              label={val}
-              ref={(ref) => (targetRef.current[index] = ref)}
+              key={tab.name}
+              label={tab.name}
+              ref={(ref) =>
+                ref !== null ? (tabRefs.current[index] = ref) : null
+              }
             />
           );
         })}
       </CustomMuiTabs>
 
       <SwipeableViews
-        index={index}
+        index={currentTabIndex}
         enableMouseEvents
         onChangeIndex={handleChangeIndex}
+        // onSwitching={handleSwitching}
       >
         {children}
       </SwipeableViews>
