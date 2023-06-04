85bd80658122eeef8c36cdc0a82ddac73e5d527f
seungwonleee <seungwon.code@gmail.com>
2023-06-04 23:30:42 +0900

    Fix: axios unexpected error handling

--- apis/errorMessage.ts
+++ apis/errorMessage.ts
@@ -4,5 +4,7 @@ import { AxiosErrorData } from '@/apis/types';
 export const errorMessage = (error: unknown) => {
   if (axios.isAxiosError<AxiosErrorData>(error)) {
     return alert(error.response?.data.message);
+  } else {
+    return alert('죄송합니다. 오류가 발생했습니다. 나중에 다시 시도해주세요.');
   }
 };
