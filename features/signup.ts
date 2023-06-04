31d143f0301e77f95147917b49cfa04251017a79
seungwonleee <seungwon.code@gmail.com>
2023-06-04 23:11:12 +0900

    Fix: signUp redux store remove authNums
    - Change parts related to authentication numbers to be handled by the server by setting redis on the server

--- features/signup.ts
+++ features/signup.ts
@@ -4,42 +4,26 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 export interface SignUpState {
   email: string;
   password: string;
-  authNums?: string;
 }
 
 const initialState: SignUpState = {
   email: '',
   password: '',
-  authNums: '',
 };
 
 const signUpSlice = createSlice({
   name: 'signup',
   initialState,
   reducers: {
-    emailAuth: (
-      state,
-      action: PayloadAction<Omit<SignUpState, 'authNums'>>,
-    ) => {
+    emailAuth: (state, action: PayloadAction<SignUpState>) => {
       const { email, password } = action.payload;
       state.email = email;
       state.password = password;
-    },
-    updateAuthNums: (
-      state,
-      action: PayloadAction<Pick<SignUpState, 'authNums'>>,
-    ) => {
-      state.authNums = action.payload.authNums;
-    },
-    resetAuth: (state) => {
-      state.email = '';
-      state.password = '';
-      state.authNums = '';
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { emailAuth, updateAuthNums, resetAuth } = signUpSlice.actions;
+export const { emailAuth } = signUpSlice.actions;
 
 export default signUpSlice.reducer;
