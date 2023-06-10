282a5d3c90371ea5fd612ea6e01544b3de7e52ad
seungwonleee <seungwon.code@gmail.com>
2023-06-10 23:13:45 +0900

    Fix: projects fetcher key for post order

--- apis/keys.ts
+++ apis/keys.ts
@@ -1,11 +1,14 @@
 export const AUTH_USER = '/api/auth/user';
 
 export const GET_PROJECTS_API_ENDPOINT = '/api/post/projects?page=1';
-export const getProjectsKey = (page?: number) => {
+export const getProjectsKey = (
+  page?: number,
+  order?: 'new' | 'popularity' | null,
+) => {
   if (!page) {
-    return `/api/post/projects?page=1`;
+    return `/api/post/projects?page=1&order=${order ?? 'new'}`;
   }
-  return `/api/post/projects?page=${page}`;
+  return `/api/post/projects?page=${page}&order=${order ?? 'new'}`;
 };
 
 /** 뒤에 /postId 를 전달해야한다. */
