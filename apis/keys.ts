b6e885de15cfb5c37508b9b46ade424eb6d58e2e
seungwonleee <seungwon.code@gmail.com>
2023-06-10 12:08:37 +0900

    Fix: get projects api key fix for pagination

--- apis/keys.ts
+++ apis/keys.ts
@@ -1,8 +1,14 @@
 export const AUTH_USER = '/api/auth/user';
 
-export const GET_PROJECTS_API_ENDPOINT = '/api/post/projects';
+export const GET_PROJECTS_API_ENDPOINT = '/api/post/projects?page=1';
+export const getProjectsKey = (page?: number) => {
+  if (!page) {
+    return `/api/post/projects?page=1`;
+  }
+  return `/api/post/projects?page=${page}`;
+};
 
 /** 뒤에 /postId 를 전달해야한다. */
 export const GET_PROJECT_DETAIL_API_ENDPOINT = '/api/post/project';
 
-export const GET_USER_PROFILE = '/api/user/profile';
+export const GET_USER_PROFILE_API_ENDPOINT = '/api/user/profile';
