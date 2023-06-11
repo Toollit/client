1573404a854ccce7bd9bde83823d1aeb75642384
seungwonleee <seungwon.code@gmail.com>
2023-06-12 07:15:45 +0900

    Add: arrow double icon

---
+++ assets/Icons/ArrowDoubleLeftIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ArrowDoubleLeft from 'public/static/icons/arrow/keyboard_double_arrow_left.svg';
+import { IconProps } from './types';
+
+const ArrowDoubleLeftIcon = ({ width = 24, height = 24 }: IconProps) => {
+  return <ArrowDoubleLeft width={width} height={height} />;
+};
+
+export default ArrowDoubleLeftIcon;
---
+++ assets/Icons/ArrowDoubleRightIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ArrowDoubleRight from 'public/static/icons/arrow/keyboard_double_arrow_right.svg';
+import { IconProps } from './types';
+
+const ArrowDoubleRightIcon = ({ width = 24, height = 24 }: IconProps) => {
+  return <ArrowDoubleRight width={width} height={height} />;
+};
+
+export default ArrowDoubleRightIcon;
