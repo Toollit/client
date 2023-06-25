f5aa0d2d386a74146b6fcbe560935a4ae209b2aa
seungwonleee <seungwon.code@gmail.com>
2023-06-26 07:51:32 +0900

    Fix: useAuth hooks multiple rendering

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,30 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import useSWR from 'swr';
 import { AUTH_USER } from '@/apis/keys';
 import { authFetcher } from '@/apis/authFetcher';
-import { useRouter } from 'next/router';
 
 /**
  * check user authentication hooks.
  */
 const useAuth = () => {
-  const router = useRouter();
-  const { data, isLoading } = useSWR(AUTH_USER, authFetcher);
+  const { data, isLoading } = useSWR(AUTH_USER, authFetcher, {
+    dedupingInterval: 60 * 1000,
+  });
 
   const authUserNickname = data?.data.nickname;
   const isAuthenticated = authUserNickname ? true : false;
   const nickname = authUserNickname ? authUserNickname : null;
   const message = data?.message ? data.message : null;
-
-  useEffect(() => {
-    //temporary password login user check
-    if (message === 'needResetPassword') {
-      // prevent infinite routing loop
-      if (router.pathname !== '/resetPassword') {
-        router.replace('/resetPassword');
-      }
-    }
-  }, [router, message]);
 
   return { isLoading, isAuthenticated, nickname, message };
 };
