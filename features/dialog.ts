31649de9dd8d4b2a33e59cd63cc0b5fb27683d78
seungwonleee <seungwon.code@gmail.com>
2023-07-01 23:23:27 +0900

    Fix: dialog redux store state

--- features/dialog.ts
+++ features/dialog.ts
@@ -2,49 +2,68 @@ import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
 export interface DialogState {
-  type: 'standard' | 'multiline' | 'select';
   open: boolean;
+  page: string;
+  type: 'standard' | 'multiline' | 'select' | null;
+  category: string;
   title: string;
   value: string;
-  newValue: string;
-  maxLength?: number;
+  maxLength?: number | null;
+  selectList?: string[] | null;
+  update: { category: string; newValue: string } | null;
 }
 
 const initialState: DialogState = {
-  type: 'standard',
   open: false,
+  page: '',
+  type: null,
+  category: '',
   title: '',
   value: '',
-  newValue: '',
-  maxLength: 0,
+  maxLength: null,
+  selectList: null,
+  update: null,
 };
 
 const dialogSlice = createSlice({
   name: 'dialog',
   initialState,
   reducers: {
-    open: (state, action: PayloadAction<Omit<DialogState, 'newValue'>>) => {
-      const { type, open, title, maxLength, value } = action.payload;
+    open: (
+      state,
+      action: PayloadAction<Omit<DialogState, 'open' | 'update'>>,
+    ) => {
+      const { page, type, title, maxLength, value, selectList } =
+        action.payload;
+      state.open = true;
+      state.page = page;
       state.type = type;
-      state.open = open;
       state.title = title;
-      state.maxLength = maxLength;
       state.value = value;
+      state.maxLength = maxLength ?? null;
+      state.selectList = type === 'select' && selectList ? selectList : null;
     },
     close: (state) => {
       state.open = false;
+      state.page = '';
+      state.type = null;
+      state.title = '';
+      state.value = '';
+      state.maxLength = null;
+      state.selectList = null;
+      state.update = null;
     },
-    updateValue: (
-      state,
-      action: PayloadAction<Pick<DialogState, 'newValue'>>,
-    ) => {
-      const { newValue } = action.payload;
-      state.newValue = newValue;
+    update: (state, action: PayloadAction<Pick<DialogState, 'update'>>) => {
+      const { update } = action.payload;
+
+      if (update !== null) {
+        state.update = update;
+      }
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { open, close, updateValue } = dialogSlice.actions;
+export const { open, close, update } = dialogSlice.actions;
 
 export default dialogSlice.reducer;
