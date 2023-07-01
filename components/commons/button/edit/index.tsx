030efffcffb174dcd0e8ee21e3e66ac4373c9478
seungwonleee <seungwon.code@gmail.com>
2023-07-01 23:19:47 +0900

    WIP: edit component for profile info update

---
+++ components/commons/button/edit/index.tsx
@@ -0,0 +1,93 @@
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { open as openDialog } from '@/features/dialog';
+import { EditButton } from './styles';
+
+interface EditBtnProps {
+  text: string;
+  page: string;
+  type: 'standard' | 'multiline' | 'select';
+  category: string;
+  title: string;
+  value: string;
+  maxLength?: number | null;
+  selectList?: string[] | null;
+  fetchFunction: () => void;
+}
+
+/**
+ * floats the dialog for modification
+ * @props text - button show text
+ * @props page - update info page
+ * @props type - dialog type 'standard' | 'multiline' | 'select'
+ * @props category - update info category
+ * @props title - dialog title
+ * @props value - dialog value
+ * @props onClick - handling dialog action
+ * @props maxLength - limit length
+ * @props selectList - selectList necessary if dialog type is 'select'
+ */
+
+const EditBtn = ({
+  text,
+  page,
+  type,
+  category,
+  title,
+  value,
+  maxLength,
+  selectList,
+  fetchFunction,
+}: EditBtnProps) => {
+  const dispatch = useDispatch();
+
+  const handleEdit = useCallback(() => {
+    // check data transferred to the component props is incorrect. value can be null or '' so exclude
+    if (!text || !page || !type || !category || !title) {
+      return;
+    }
+
+    if (type === 'standard') {
+      return dispatch(
+        openDialog({
+          page,
+          type,
+          category,
+          title,
+          value,
+          maxLength,
+        }),
+      );
+    }
+
+    if (type === 'multiline') {
+      return dispatch(
+        openDialog({
+          page,
+          type,
+          category,
+          title,
+          value,
+          maxLength,
+        }),
+      );
+    }
+
+    if (type === 'select') {
+      return dispatch(
+        openDialog({
+          page,
+          type,
+          category,
+          title,
+          value,
+          selectList,
+        }),
+      );
+    }
+  }, [dispatch, text, category, type, title, value, maxLength, selectList]);
+
+  return <EditButton onClick={handleEdit}>{text}</EditButton>;
+};
+
+export default EditBtn;
---
+++ components/commons/button/edit/styles.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const EditButton = styled.button`
+  font-size: 1.4rem;
+  display: inline-block;
+  min-width: 4.8rem;
+  height: 2.4rem;
+  padding: 0.2rem 0.9rem;
+  border-radius: 0.4rem;
+  border: solid 1px rgba(212, 216, 229, 0.5);
+  background-color: rgba(212, 216, 229, 0.25);
+  text-align: center;
+  color: #7b8994;
+`;
+
+export { EditButton };
