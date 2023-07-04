2039338fcb3313fa2888e860856b934bee8a74d6
seungwonleee <seungwon.code@gmail.com>
2023-07-04 22:50:45 +0900

    Add: profile additional info box complete

--- models/profile/ProfileInfoBox.tsx
+++ models/profile/ProfileInfoBox.tsx
@@ -139,7 +139,11 @@ const ProfileInfoBox = ({ me, data }: ProfileInfoViewProps) => {
           <CategoryContent>
             <div>
               <Text>온/오프라인:</Text>
-              <Text padding>{data.onOffline ?? '미입력'}</Text>
+              <Text padding>
+                {data.onOffline === null || data.onOffline.length === 0
+                  ? '미입력'
+                  : data.onOffline}
+              </Text>
             </div>
             {me && (
               <EditBtn
@@ -149,76 +153,112 @@ const ProfileInfoBox = ({ me, data }: ProfileInfoViewProps) => {
                 type={'select'}
                 title={'온/오프라인'}
                 value={data.onOffline ?? ''}
-                selectList={['온라인', '오프라인']}
+                selectList={[
+                  '온라인 가능',
+                  '오프라인 가능',
+                  '온,오프라인 모두 가능',
+                ]}
               />
             )}
           </CategoryContent>
           <CategoryContent>
             <div>
-              <Text>지역:</Text>
-              <Text padding>{data.place ?? '미입력'}</Text>
+              <Text>모임장소:</Text>
+              <Text padding>
+                {data.place === null || data.place.length === 0
+                  ? '미입력'
+                  : data.place}
+              </Text>
             </div>
             {me && (
               <EditBtn
                 text={'수정'}
                 page={'profile'}
-                category={'local'}
-                type={'select'}
-                title={'지역'}
+                category={'place'}
+                type={'standard'}
+                title={'모임 장소'}
                 value={data.place ?? ''}
-                selectList={['서울', '경기도']} // TODO 구글 지도에서 위치 찍어서 표시하도록하기? 주소검색?
+                placeholder='ex) 서울특별시 종로구, 상관없음'
+                maxLength={30}
               />
             )}
           </CategoryContent>
           <CategoryContent>
             <div>
-              <Text>가능시간:</Text>
-              <Text padding>{data.contactTime ?? '미입력'}</Text>
-              {/* 주중,주말 가능/시간대 미정 */}
+              <Text>모임시간:</Text>
+              <Text padding>
+                {data.contactTime === null || data.contactTime.length === 0
+                  ? '미입력'
+                  : data.contactTime}
+              </Text>
             </div>
             {me && (
               <EditBtn
                 text={'수정'}
                 page={'profile'}
-                category={''}
-                type={'select'}
-                title={'가능시간'}
+                category={'contactTime'}
+                type={'standard'}
+                title={'모임시간'}
                 value={data.contactTime ?? ''}
+                placeholder={'ex) 평일 9시~18시, 화요일 20시 이후'}
+                maxLength={30}
               />
             )}
           </CategoryContent>
           <CategoryContent>
             <div>
               <Text>관심분야:</Text>
-              <Text padding>{data.interests ?? '미입력'}</Text>
-              {/* 공유서비스, O2O, 이커머스, 유틸, 금융 */}
+              <Text padding>
+                {data.interests === null || data.interests.length === 0
+                  ? '미입력'
+                  : data.interests}
+              </Text>
             </div>
             {me && (
               <EditBtn
                 text={'수정'}
                 page={'profile'}
-                category={''}
-                type={'select'}
+                category={'interests'}
+                type={'multiSelect'}
                 title={'관심분야'}
                 value={data.interests ?? ''}
-                selectList={['1', '2', '3']}
+                selectList={[
+                  '공유서비스',
+                  '이커머스',
+                  '유틸',
+                  '금융',
+                  '헬스케어',
+                  '소셜네트워크',
+                  '여행',
+                  '뉴스/정보',
+                  '게임',
+                  '엔터테인먼트',
+                  '의료/병원',
+                  '기타',
+                ]}
+                maxLength={20}
               />
             )}
           </CategoryContent>
           <CategoryContent>
             <div>
-              <Text>경력사항:</Text>
-              <Text padding>{data.career ?? '미입력'}</Text>
-              {/* 1년차 취준생, 학생, 기타 */}
+              <Text>직무/경력:</Text>
+              <Text padding>
+                {data.career === null || data.career.length === 0
+                  ? '미입력'
+                  : data.career}
+              </Text>
             </div>
             {me && (
               <EditBtn
                 text={'수정'}
                 page={'profile'}
-                category={''}
-                type={'select'}
+                category={'career'}
+                type={'standard'}
                 title={'경력사항'}
                 value={data.career ?? ''}
+                placeholder={'ex) 3년차 개발자, 1년차 디자이너, 학생'}
+                maxLength={30}
               />
             )}
           </CategoryContent>
