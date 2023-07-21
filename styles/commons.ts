442e71f711d84557be75b92489e87e4be827fb63
seungwonleee <seungwon.code@gmail.com>
2023-07-21 21:17:57 +0900

    Refactor: svg container common style div to span
    - change div tag to span tag and add display inline-block for content size settings

--- styles/commons.ts
+++ styles/commons.ts
@@ -5,7 +5,8 @@ interface SVGContainerProps {
   height: number;
 }
 
-const SVGContainer = styled.div<SVGContainerProps>`
+const SVGContainer = styled.span<SVGContainerProps>`
+  display: inline-block;
   width: ${(props) => `${props.width * 0.1}rem`};
   height: ${(props) => `${props.height * 0.1}rem`};
 `;
