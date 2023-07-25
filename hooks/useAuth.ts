fca70c70a250dfffbe1a0d4467e4f8221974e030
seungwonleee <seungwon.code@gmail.com>
2023-07-26 04:38:36 +0900

    Refactor: useAuth hooks useSWR to useSWRImmutable

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,23 +1,20 @@
 import React from 'react';
-import useSWR from 'swr';
-import { AUTH_USER } from '@/apis/keys';
+import useSWRImmutable from 'swr/immutable';
+import { authUserKey } from '@/apis/keys';
 import { authFetcher } from '@/apis/authFetcher';
 
 /**
  * check user authentication hooks.
  */
 const useAuth = () => {
-  const { data, isLoading } = useSWR(AUTH_USER, authFetcher, {
-    dedupingInterval: 60 * 1000,
-    revalidateOnFocus: false,
-  });
+  const { data, isLoading, mutate } = useSWRImmutable(authUserKey, authFetcher);
 
   const authUserNickname = data?.data.nickname;
   const isAuthenticated = authUserNickname ? true : false;
   const nickname = authUserNickname ? authUserNickname : null;
   const message = data?.message ? data.message : null;
 
-  return { isLoading, isAuthenticated, nickname, message };
+  return { isLoading, isAuthenticated, nickname, message, mutate };
 };
 
 export default useAuth;
