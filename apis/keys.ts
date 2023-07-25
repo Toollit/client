9c711a3eaffb1a227e2248841c5af084e2d322ae
seungwonleee <seungwon.code@gmail.com>
2023-07-26 07:08:09 +0900

    Refactor: fetcher keys

--- apis/keys.ts
+++ apis/keys.ts
@@ -1,4 +1,8 @@
-export const AUTH_USER = '/api/auth/user';
+const AUTH_USER_API_ENDPOINT = '/api/auth/user';
+
+export const authUserKey = (): string => {
+  return AUTH_USER_API_ENDPOINT;
+};
 
 export const GET_PROJECTS_API_ENDPOINT = '/api/post/projects?page=1';
 export const getProjectsKey = (page: number, order: 'new' | 'popularity') => {
@@ -8,4 +12,17 @@ export const getProjectsKey = (page: number, order: 'new' | 'popularity') => {
 /** 뒤에 /postId 를 전달해야한다. */
 export const GET_PROJECT_DETAIL_API_ENDPOINT = '/api/post/project';
 
-export const GET_USER_PROFILE_API_ENDPOINT = '/api/user/profile';
+// profile page
+const PROFILE_API_ENDPOINT = '/api/user/profile';
+
+export const getProfileImageKey = (nickname: string) => {
+  return `${PROFILE_API_ENDPOINT}/${nickname}`;
+};
+
+export const getProfileInfoKey = (nickname: string) => {
+  return `${PROFILE_API_ENDPOINT}/${nickname}?tab=viewProfile`;
+};
+
+export const getProfileProjectsKey = (nickname: string, count: number) => {
+  return `${PROFILE_API_ENDPOINT}/${nickname}?tab=viewProjects&count=${count}`;
+};
