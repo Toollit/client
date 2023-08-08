1e59d7f6aea07dd800f894b592a80e057cadac65
seungwonleee <seungwon.code@gmail.com>
2023-08-08 18:47:45 +0900

    Add: project detail bookmark, share project handling

--- models/project/ProjectDetailController.tsx
+++ models/project/ProjectDetailController.tsx
@@ -1,37 +1,49 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import ProjectDetailView, { ProjectDetailViewProps } from './ProjectDetailView';
 import { changeDateFormat, dateFromNow } from '@/utils/changeDateFormat';
 import { useRouter } from 'next/router';
-import LoadingCircularProgress from '@/components/commons/loading';
 import useSWR from 'swr';
 import {
   ProjectDetailAPIRes,
   projectDetailFetcher,
 } from '@/apis/projectDetailFetcher';
-import { getProjectDetailKey } from '@/apis/keys';
+import { getCheckBookmarkKey, getProjectDetailKey } from '@/apis/keys';
 import { errorMessage } from '@/apis/errorMessage';
 import useAuth from '@/hooks/useAuth';
+import { useDispatch } from 'react-redux';
+import { showAlert } from '@/features/alert';
+import { bookmarkAPI } from '@/apis/bookmark';
+import { checkBookmarkFetcher } from '@/apis/checkBookmarkFetcher';
 
 type CustomMemberTypes = ('Developer' | 'Designer' | 'PM' | 'Anyone')[];
 
 const ProjectDetailController = () => {
-  const [isClientRendering, setIsClientRendering] = useState(false);
-
-  useEffect(() => {
-    setIsClientRendering(true);
-  }, []);
-
-  // Current Access User Self Information
-  const { nickname: accessUser } = useAuth();
-
+  const dispatch = useDispatch();
   const router = useRouter();
   const postId = router.query.id as string;
+
+  // Current Access User Self Information
+  const { nickname: accessUser, authMutate } = useAuth();
+
+  const [isClientRendering, setIsClientRendering] = useState(false);
 
   const { data: projectDetail } = useSWR(
     postId ? getProjectDetailKey(postId) : null,
     projectDetailFetcher,
     {
       revalidateOnMount: false,
+      revalidateOnFocus: false,
+      errorRetryCount: 0,
+      onError(err, key, config) {
+        errorMessage(err);
+      },
+    },
+  );
+
+  const { data: bookmark, mutate: bookmarkMutate } = useSWR(
+    postId ? getCheckBookmarkKey(postId) : null,
+    checkBookmarkFetcher,
+    {
       revalidateOnFocus: false,
       errorRetryCount: 0,
       onError(err, key, config) {
@@ -53,39 +65,97 @@ const ProjectDetailController = () => {
     [],
   );
 
-  if (!projectDetail) {
-    return <LoadingCircularProgress />;
-  }
+  const handleBookmark = useCallback(async () => {
+    const isMyProject = projectDetail?.data.writer.nickname === accessUser;
+
+    const auth = await authMutate();
+
+    if (!auth?.success) {
+      alert('로그인 후 이용 가능합니다.');
+      return router.push('/login');
+    }
+
+    if (isMyProject) {
+      return alert('내가 작성한 게시글 입니다.');
+    }
+
+    try {
+      const response = await bookmarkAPI({ postId: Number(postId) });
+
+      bookmarkMutate();
+
+      if (response?.message === 'save') {
+        dispatch(showAlert({ type: 'success', text: '북마크 했습니다.' }));
+      }
+
+      if (response?.message === 'cancel') {
+        dispatch(
+          showAlert({ type: 'success', text: '북마크를 취소했습니다.' }),
+        );
+      }
+    } catch (error) {
+      errorMessage(error);
+    }
+  }, [
+    projectDetail,
+    accessUser,
+    dispatch,
+    postId,
+    router,
+    authMutate,
+    bookmarkMutate,
+  ]);
+
+  const handleShare = useCallback(() => {
+    const fullUrl = process.env.NEXT_PUBLIC_CLIENT_HOST + router.asPath;
+
+    navigator.clipboard.writeText(fullUrl);
+
+    dispatch(showAlert({ type: 'info', text: '주소가 복사되었습니다.' }));
+  }, [dispatch, router]);
+
+  useEffect(() => {
+    setIsClientRendering(true);
+  }, []);
 
   const props: ProjectDetailViewProps = {
     isClientRendering,
-    writer: {
-      nickname: projectDetail.writer.nickname,
-      lastLoginAt: dateFromNow({
-        date: projectDetail.writer.lastLoginAt,
-      }),
-      profileImage: projectDetail.writer.profileImage,
-    },
-    content: {
-      title: projectDetail.content.title,
-      contentHTML: projectDetail.content.contentHTML,
-      contentMarkdown: projectDetail.content.contentMarkdown,
-      views: projectDetail.content.views,
-      createdAt: changeDateFormat({
-        date: projectDetail.content.createdAt,
-        format: 'YYMMDD_hhmm',
-      }),
-      updatedAt: changeDateFormat({
-        date: projectDetail.content.updatedAt,
-        format: 'YYMMDD_hhmm',
-      }),
-      hashtags: projectDetail.content.hashtags,
-      memberTypes: handleMemberTypes(projectDetail.content.memberTypes),
-      recruitNumber: projectDetail.content.recruitNumber,
-    },
-    me: {
-      nickname: accessUser,
-    },
+    me: projectDetail?.data.writer.nickname === accessUser,
+    postId: postId,
+    writer: projectDetail
+      ? {
+          nickname: projectDetail.data.writer.nickname,
+          lastLoginAt: dateFromNow({
+            date: projectDetail.data.writer.lastLoginAt,
+          }),
+          profileImage: projectDetail.data.writer.profileImage,
+        }
+      : projectDetail,
+    content: projectDetail
+      ? {
+          title: projectDetail.data.content.title,
+          contentHTML: projectDetail.data.content.contentHTML,
+          contentMarkdown: projectDetail.data.content.contentMarkdown,
+          views: projectDetail.data.content.views,
+          createdAt: changeDateFormat({
+            date: projectDetail.data.content.createdAt,
+            format: 'YYMMDD_hhmm',
+          }),
+          updatedAt: changeDateFormat({
+            date: projectDetail.data.content.updatedAt,
+            format: 'YYMMDD_hhmm',
+          }),
+          hashtags: projectDetail.data.content.hashtags,
+          memberTypes: handleMemberTypes(
+            projectDetail.data.content.memberTypes,
+          ),
+          recruitNumber: projectDetail.data.content.recruitNumber,
+        }
+      : projectDetail,
+
+    bookmark: bookmark?.data.bookmark,
+    handleBookmark,
+    handleShare,
 
     //TODO comment 추가하기
     //TODO trending post 추가하기
