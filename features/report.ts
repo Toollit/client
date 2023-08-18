03ca7b56230669fa6d8587efa1d6f1e0619c9575
seungwonleee <seungwon.code@gmail.com>
2023-08-18 23:39:54 +0900

    Fix: report redux state for view

--- features/report.ts
+++ features/report.ts
@@ -1,22 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 interface InitialState {
   open: boolean;
+  postType: null | string;
+  postId: null | number;
+  writer: null | string;
+  title: null | string;
 }
 
 const initialState: InitialState = {
   open: false,
+  postType: null,
+  postId: null,
+  writer: null,
+  title: null,
 };
 
 const reportSlice = createSlice({
   name: 'report',
   initialState,
   reducers: {
-    openReport: (state) => {
+    openReport: (state, action: PayloadAction<Omit<InitialState, 'open'>>) => {
+      const { postType, postId, writer, title } = action.payload;
       state.open = true;
+      state.postType = postType;
+      state.postId = postId;
+      state.writer = writer;
+      state.title = title;
     },
     closeReport: (state) => {
       state.open = false;
+      state.postType = null;
+      state.postId = null;
+      state.writer = null;
+      state.title = null;
     },
   },
 });
