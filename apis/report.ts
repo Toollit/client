d1e03be38d1ad075c19b9f86f90025c60073a331
seungwonleee <seungwon.code@gmail.com>
2023-08-19 10:59:17 +0900

    Add: report api

---
+++ apis/report.ts
@@ -0,0 +1,22 @@
+import { serverInstance } from 'apis/axios';
+
+export interface ReportAPIReq {
+  postId: number;
+  postType: string;
+  title: string;
+  writer: string;
+  reason: string;
+  url: string;
+}
+
+export interface ReportAPIRes {
+  success: boolean;
+  message: null | string;
+}
+
+export const reportAPI = async (
+  data: ReportAPIReq,
+): Promise<ReportAPIRes | undefined> => {
+  const response = await serverInstance.post('/api/post/report', data);
+  return response.data;
+};
