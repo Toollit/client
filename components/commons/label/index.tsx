5719cb60c198255591690f6e82b72fc644f709d9
seungwonleee <seungwon.code@gmail.com>
2023-08-26 22:00:56 +0900

    Refactor: Label component for more commons

--- components/commons/label/index.tsx
+++ components/commons/label/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyledLabel } from './styles';
 
 interface LabelProps {
-  htmlFor: string;
+  htmlFor?: string;
   text: string;
 }
 
--- components/commons/label/styles.tsx
+++ components/commons/label/styles.tsx
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled';
 
 const StyledLabel = styled.label`
+  display: inline-block;
+  width: 100%;
   font-size: 1.8rem;
   font-weight: 500;
-  margin-left: 0.5rem;
+  padding: 0.5rem;
 `;
 
 export { StyledLabel };
