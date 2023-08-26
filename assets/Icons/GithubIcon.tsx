56d3ec9fcdc71677ed71a3134d75f4c389e419cc
seungwonleee <seungwon.code@gmail.com>
2023-08-26 21:58:32 +0900

    Fix: github, google icon customize

--- assets/Icons/GithubIcon.tsx
+++ assets/Icons/GithubIcon.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { IconProps } from './types';
+import { SVGContainer } from '@/styles/commons';
 import GithubSvg from 'public/static/icons/social/Github.svg';
 
-const GithubIcon = () => {
-  return <GithubSvg />;
+const GithubIcon = ({
+  fill = false,
+  width = 24,
+  height = 24,
+  color,
+}: IconProps) => {
+  return (
+    <SVGContainer width={width} height={height}>
+      <GithubSvg width={'100%'} height={'100%'} fill={color} />
+    </SVGContainer>
+  );
 };
 
 export default GithubIcon;
--- assets/Icons/GoogleIcon.tsx
+++ assets/Icons/GoogleIcon.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { IconProps } from './types';
+import { SVGContainer } from '@/styles/commons';
 import GoogleSvg from 'public/static/icons/social/Google.svg';
 
-const GoogleIcon = () => {
-  return <GoogleSvg />;
+const GoogleIcon = ({
+  fill = false,
+  width = 24,
+  height = 24,
+  color,
+}: IconProps) => {
+  return (
+    <SVGContainer width={width} height={height}>
+      <GoogleSvg width={'100%'} height={'100%'} fill={color} />
+    </SVGContainer>
+  );
 };
 
 export default GoogleIcon;
