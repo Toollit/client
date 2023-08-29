c8dfce4afedc91655c2f810e8d5f876083146e5b
seungwonleee <seungwon.code@gmail.com>
2023-08-30 03:43:47 +0900

    Refactor: serialize middleware swr key format Multiple Arguments -> Passing Objects

--- middleware/swr/serialize.ts
+++ middleware/swr/serialize.ts
@@ -3,12 +3,12 @@ import useSWR, { Middleware, SWRHook } from 'swr';
 export const serialize: Middleware =
   (useSWRNext: SWRHook) => (key, fetcher, config) => {
     // Serialize the key.
-    const serializedKey = Array.isArray(key) ? JSON.stringify(key) : key;
+    const serializedKey = typeof key === 'string' ? key : JSON.stringify(key);
 
     // Pass the serialized key, and unserialize it in fetcher.
     return useSWRNext(
       serializedKey,
-      fetcher ? (k: string) => fetcher(...JSON.parse(k)) : null,
+      fetcher ? (k: string) => fetcher({ ...JSON.parse(k) }) : null,
       config,
     );
   };
