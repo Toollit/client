5c8255fbd12b40eb233603bfbff6061060a6d318
seungwonleee <seungwon.code@gmail.com>
2023-09-01 23:33:07 +0900

    WIP: useCachedKeys hooks
    - fix clearCache hanlder

--- hooks/useCachedKeys.ts
+++ hooks/useCachedKeys.ts
@@ -1,32 +1,78 @@
 import React, { useCallback, useRef, useEffect } from 'react';
 import { useSWRConfig } from 'swr';
 
+interface CachedKeysRefs {
+  key: string;
+  url: string;
+  args: { page: string; tag: string };
+}
+
 /**
- * handle swr cache key hooks
+ * swr cache keys handle hooks
  */
 const useCachedKeys = () => {
   const { mutate, cache } = useSWRConfig();
 
-  const cachedKeysRefs = useRef<string[]>([]);
+  const cachedKeysRefs = useRef<CachedKeysRefs[]>([]);
 
   const clearCache = useCallback(() => {
-    for (let i = 0; i < cachedKeysRefs.current.length; i++) {
-      // revalidate all keys
-      mutate(cachedKeysRefs.current[i], undefined, true);
-    }
-  }, [mutate]);
-
-  useEffect(() => {
     const cachedKeys = cache.keys();
 
     for (let key of cachedKeys) {
-      cachedKeysRefs.current = [...cachedKeysRefs.current, key];
+      cache.delete(key);
     }
+  }, [cache]);
+
+  const revalidateCache = useCallback(
+    ({ page, tag }: { page?: string; tag?: string }) => {
+      const keysValueObj = cachedKeysRefs.current;
+
+      if (page) {
+        const filtered = keysValueObj.filter((v) => v.args?.page === page);
+
+        filtered.forEach((v) => {
+          mutate(v.key, undefined, true);
+        });
+      }
+
+      if (tag) {
+        const filtered = keysValueObj.filter((v) => v.args?.tag === tag);
+
+        filtered.forEach((v) => {
+          mutate(v.key, undefined, true);
+        });
+      }
+    },
+    [mutate],
+  );
+
+  useEffect(() => {
+    const cachedKeys = cache.keys();
+    // console.log('cachedKeysRefs.current ===>', cachedKeys);
+    // for (let key of cachedKeys) {
+    //   cachedKeysRefs.current = [...cachedKeysRefs.current, key];
+    // }
+
+    // for (let key of cachedKeys) {
+    //   console.log(JSON.parse(key));
+    // }
+
+    const keys = Array.from(cachedKeys);
+
+    for (let i = 0; i < keys.length; i++) {
+      cachedKeysRefs.current = [
+        ...cachedKeysRefs.current,
+        { key: keys[i], ...JSON.parse(keys[i]) },
+      ];
+    }
+
+    // console.log('cachedKeysRefs.current ===>', cachedKeys);
   }, [cache]);
 
   return {
     cachedKeys: cachedKeysRefs.current,
     clearCache,
+    revalidateCache,
   };
 };
 
