b1c1a5b92a6a38a1fdace03a70e3d3c1d12ff80c
seungwonleee <seungwon.code@gmail.com>
2023-09-04 04:05:26 +0900

    Add: useCachedKeys hooks add function getCustomCachedKeys, getCachedKeyWithTag, getCachedDataWithKey
    - remove duplication of logic

--- hooks/useCachedKeys.ts
+++ hooks/useCachedKeys.ts
@@ -1,13 +1,13 @@
 import React, { useCallback, useRef, useEffect } from 'react';
 import { useSWRConfig } from 'swr';
 
-interface CachedKeysRefs {
+interface CustomCachedKeys {
   key: string;
   url: string;
   args: { page: string; tag: string };
 }
 
-interface MutateProps {
+interface FunctionProps {
   [key: string]: string;
 }
 
@@ -17,8 +17,6 @@ interface MutateProps {
 const useCachedKeys = () => {
   const { mutate, cache } = useSWRConfig();
 
-  const cachedKeysRefs = useRef<CachedKeysRefs[]>([]);
-
   const clearCache = useCallback(() => {
     const cachedKeys = cache.keys();
 
@@ -27,9 +25,43 @@ const useCachedKeys = () => {
     }
   }, [cache]);
 
+  const getCustomCachedKeys = useCallback(() => {
+    const cachedKeys = cache.keys();
+
+    const keys = Array.from(cachedKeys);
+
+    let customCachedKeys: CustomCachedKeys[] = [];
+
+    for (let i = 0; i < keys.length; i++) {
+      customCachedKeys.push({ key: keys[i], ...JSON.parse(keys[i]) });
+    }
+
+    return customCachedKeys;
+  }, [cache]);
+
+  const getCachedKeyWithTag = useCallback(
+    ({ tag }: FunctionProps) => {
+      const found = getCustomCachedKeys().find((key) => key.args?.tag === tag);
+
+      const key = found?.key;
+
+      return key;
+    },
+    [getCustomCachedKeys],
+  );
+
+  const getCachedDataWithKey = useCallback(
+    ({ key }: FunctionProps) => {
+      const cachedData = cache.get(key)?.data?.data;
+
+      return cachedData;
+    },
+    [cache],
+  );
+
   const mutateCachedKeysWithTag = useCallback(
-    ({ tag }: MutateProps) => {
-      const found = cachedKeysRefs.current.filter(
+    ({ tag }: FunctionProps) => {
+      const found = getCustomCachedKeys().filter(
         (key) => key.args?.tag === tag,
       );
 
@@ -37,12 +69,12 @@ const useCachedKeys = () => {
         return mutate(v.key, undefined, true);
       });
     },
-    [mutate],
+    [mutate, getCustomCachedKeys],
   );
 
   const mutateCachedKeysWithPage = useCallback(
-    ({ page }: MutateProps) => {
-      const found = cachedKeysRefs.current.filter(
+    ({ page }: FunctionProps) => {
+      const found = getCustomCachedKeys().filter(
         (key) => key.args?.page === page,
       );
 
@@ -50,34 +82,16 @@ const useCachedKeys = () => {
         return mutate(v.key, undefined, true);
       });
     },
-    [mutate],
+    [mutate, getCustomCachedKeys],
   );
 
-  const getCachedKeyWithTag = useCallback(({ tag }: MutateProps) => {
-    const found = cachedKeysRefs.current.find((key) => key.args?.tag === tag);
-
-    return found;
-  }, []);
-
-  useEffect(() => {
-    const cachedKeys = cache.keys();
-
-    const keys = Array.from(cachedKeys);
-
-    for (let i = 0; i < keys.length; i++) {
-      cachedKeysRefs.current = [
-        ...cachedKeysRefs.current,
-        { key: keys[i], ...JSON.parse(keys[i]) },
-      ];
-    }
-  }, [cache]);
-
   return {
-    cachedKeys: cachedKeysRefs.current,
     clearCache,
+    getCustomCachedKeys,
+    getCachedKeyWithTag,
+    getCachedDataWithKey,
     mutateCachedKeysWithTag,
     mutateCachedKeysWithPage,
-    getCachedKeyWithTag,
   };
 };
 
