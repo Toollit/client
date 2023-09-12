31774b597228fe0b46abc1504eb8d5c3ad85d405
seungwonleee <seungwon.code@gmail.com>
2023-09-13 01:05:21 +0900

    Add: tooltip component tooltip position props and annotation

--- components/commons/tooltip/index.tsx
+++ components/commons/tooltip/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TooltipBox, Item, Content } from './styles';
+import type { PopoverOrigin } from '@mui/material';
 
 export interface TooltipProps {
   items: {
@@ -10,18 +11,36 @@ export interface TooltipProps {
   anchorEl: null | HTMLElement;
   open: boolean;
   onClose: () => void;
+  position?: PopoverOrigin;
 }
 
 /**
- * Tooltip for managing contents. To be used directly below the button components
+ * Tooltip for managing contents.
+ * To be used directly below the button tag.
+ * Only use with button tag. When used with other tags, the button appears to be pressed.
  * @props items - tooltip select list
  * @props anchorEl - anchorEl to be used to control tooltip
  * @props open - Used to open tooltip
  * @props onClose - Used to close tooltip
+ * @props position - tooltip display position
  */
-const Tooltip = ({ items, anchorEl, open, onClose }: TooltipProps) => {
+const Tooltip = ({
+  items,
+  anchorEl,
+  open,
+  onClose,
+  position = {
+    vertical: 'center',
+    horizontal: 'center',
+  },
+}: TooltipProps) => {
   return (
-    <TooltipBox anchorEl={anchorEl} open={open} onClose={onClose}>
+    <TooltipBox
+      anchorEl={anchorEl}
+      open={open}
+      onClose={onClose}
+      anchorOrigin={position}
+    >
       {items?.map((item) => {
         return (
           <Item key={item.text} onClick={item.handler}>
