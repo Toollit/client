6931f737d6a7249916ff4f9aa9c4840eb5ede1b2
seungwonleee <seungwon.code@gmail.com>
2023-09-18 14:34:50 +0900

    Refactor: loading circular progress

--- components/commons/loading/index.tsx
+++ components/commons/loading/index.tsx
@@ -4,50 +4,58 @@ import CircularProgress, {
   circularProgressClasses,
   CircularProgressProps,
 } from '@mui/material/CircularProgress';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store';
 
 export default function LoadingCircularProgress(props: CircularProgressProps) {
-  return (
-    <Box
-      sx={{
-        position: 'fixed',
-        left: '0',
-        right: '0',
-        top: '0',
-        bottom: '0',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(15, 20, 25, 0.1)',
-        zIndex: 100,
-      }}
-    >
-      <CircularProgress
-        variant='determinate'
+  const isLoading = useSelector((state: RootState) => state.isLoading.status);
+
+  if (isLoading) {
+    return (
+      <Box
         sx={{
-          color: (theme) =>
-            theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+          position: 'fixed',
+          left: '0',
+          right: '0',
+          top: '0',
+          bottom: '0',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'rgba(15, 20, 25, 0.1)',
+          zIndex: 100,
         }}
-        size={40}
-        thickness={4}
-        {...props}
-        value={100}
-      />
-      <CircularProgress
-        variant='indeterminate'
-        disableShrink
-        sx={{
-          color: '#4dd290',
-          animationDuration: '550ms',
-          position: 'absolute',
-          // left: 0,
-          [`& .${circularProgressClasses.circle}`]: {
-            strokeLinecap: 'round',
-          },
-        }}
-        size={40}
-        thickness={4}
-        {...props}
-      />
-    </Box>
-  );
+      >
+        <CircularProgress
+          variant='determinate'
+          sx={{
+            color: (theme) =>
+              theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+          }}
+          size={40}
+          thickness={4}
+          {...props}
+          value={100}
+        />
+        <CircularProgress
+          variant='indeterminate'
+          disableShrink
+          sx={{
+            color: '#4dd290',
+            animationDuration: '550ms',
+            position: 'absolute',
+            // left: 0,
+            [`& .${circularProgressClasses.circle}`]: {
+              strokeLinecap: 'round',
+            },
+          }}
+          size={40}
+          thickness={4}
+          {...props}
+        />
+      </Box>
+    );
+  } else {
+    return <></>;
+  }
 }
