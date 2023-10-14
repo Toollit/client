5349e3aa1451fffd6e9e08a337225c80fddc46b5
seungwonleee <seungwon.code@gmail.com>
2023-10-14 21:56:33 +0900

    Refactor: project fetcher

---
+++ apis/projectFetcher.ts
@@ -0,0 +1,50 @@
+import { serverInstance } from './axios';
+import { FetcherParams } from './types';
+
+export interface ProjectWriter {
+  nickname: string;
+  lastLoginAt: string;
+  profileImage: string | null;
+}
+
+export interface ProjectContent {
+  title: string;
+  contentHTML: string;
+  contentMarkdown: string;
+  views: number;
+  createdAt: string;
+  updatedAt: string | null;
+  hashtags: string[];
+  memberTypes: ('developer' | 'designer' | 'pm' | 'anyone')[];
+  recruitNumber: number;
+  representativeImage: string | null;
+}
+
+export interface ProjectMember {
+  profiles: {
+    nickname: string;
+    profileImage: string | null;
+  }[];
+}
+
+export interface ProjectDetail {
+  writer: ProjectWriter;
+  content: ProjectContent;
+  member: ProjectMember;
+}
+
+export interface ProjectAPIReq {}
+
+export interface ProjectAPIRes {
+  success: boolean;
+  message: string | null;
+  data: ProjectDetail;
+}
+
+export const projectFetcher = async ({
+  url,
+  config,
+}: FetcherParams): Promise<ProjectAPIRes | undefined> => {
+  const response = await serverInstance.get(url, config ? { ...config } : {});
+  return response.data;
+};
