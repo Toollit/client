5a8e0711de26624bb44883ae3f72ddc9f0a4350a
seungwonleee <seungwon.code@gmail.com>
2023-10-17 21:33:17 +0900

    Style: Footer component styles complete
    - Link href needs to be added

--- components/appLayout/footer/index.tsx
+++ components/appLayout/footer/index.tsx
@@ -1,41 +1,80 @@
 import React from 'react';
-import Block from '@/components/commons/block';
+import Link from 'next/link';
+import { InnerContainer } from '@/styles/commons';
 import {
   AsideArea,
-  CenterContainer,
   Container,
   CorpArea,
+  DeveloperContainer,
   NoticeArea,
+  PartnersContainer,
 } from './styles';
 
 const Footer = () => {
   return (
     <Container>
-      <CenterContainer>
-        <Block paddingLeft={1.5} paddingRight={1.5} paddingTop={2}>
-          <NoticeArea>공지사항</NoticeArea>
-        </Block>
-        <Block paddingLeft={1.5} paddingRight={1.5} paddingTop={2}>
-          <AsideArea>
-            <div>Creators</div>
-            <div>Partners</div>
-          </AsideArea>
-        </Block>
-        <Block paddingLeft={1.5} paddingRight={1.5} paddingTop={2}>
-          <CorpArea>
-            <ul>
-              <li>회사소개</li>
-              <li>인재채용</li>
-              <li>제휴제한</li>
-              <li>이용약관</li>
-              <li>개인정보처리방침</li>
-              <li>Getit 정책</li>
-              <li>고객센터</li>
-              <li>ⓒ Getit Corp.</li>
-            </ul>
-          </CorpArea>
-        </Block>
-      </CenterContainer>
+      <InnerContainer>
+        <NoticeArea>
+          <Link href=''>
+            <a>공지사항</a>
+          </Link>
+        </NoticeArea>
+
+        <AsideArea>
+          <PartnersContainer>
+            <h3>Partners</h3>
+            <Link href=''>
+              <a>비즈니스 · 광고</a>
+            </Link>
+          </PartnersContainer>
+          <DeveloperContainer>
+            <h3>Developer</h3>
+            <Link href=''>
+              <a>Github</a>
+            </Link>
+            <Link href=''>
+              <a>Blog</a>
+            </Link>
+          </DeveloperContainer>
+        </AsideArea>
+
+        <CorpArea>
+          <ul>
+            <li>
+              <Link href={''}>
+                <a>회사소개</a>
+              </Link>
+            </li>
+            {/* <li>인재채용</li> */}
+            <li>
+              <Link href={''}>
+                <a>제휴제안</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/policy/terms-of-service'}>
+                <a>이용약관</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/policy/privacy'}>
+                <a>개인정보처리방침</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={''}>
+                <a>Getit 정책</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={''}>
+                <a>고객센터</a>
+              </Link>
+            </li>
+            <li>ⓒ Getit Corp.</li>
+          </ul>
+        </CorpArea>
+      </InnerContainer>
     </Container>
   );
 };
--- components/appLayout/footer/styles.ts
+++ components/appLayout/footer/styles.ts
@@ -1,36 +1,147 @@
+import { mediaQueryLaptop } from '@/styles/mediaQuery';
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
 
 const Container = styled.div`
   background-color: rgb(245, 246, 247);
-  min-height: 20rem;
-
+  min-height: 30rem;
   width: 100%;
   margin-top: auto;
 `;
 
-const CenterContainer = styled.div`
-  max-width: 102.4rem;
-  width: 100%;
-  margin: 0 auto;
-`;
-
 const NoticeArea = styled.div`
-  padding-bottom: 2rem;
+  padding: 4rem 1.5rem 2rem 1.5rem;
   border-bottom: 1px solid ${(props) => props.theme.colors.border.divider};
-`;
+  font-size: 1.3rem;
 
-const AsideArea = styled.div`
-  padding-bottom: 2rem;
-  border-bottom: 1px solid ${(props) => props.theme.colors.border.divider};
-`;
+  a {
+    font-size: 1.3rem;
+    color: #000;
+    font-weight: 500;
 
-const CorpArea = styled.div`
-  padding-bottom: 2rem;
-
-  ul {
-    display: flex;
+    :hover {
+      text-decoration-line: underline;
+    }
   }
 `;
 
-export { Container, CenterContainer, NoticeArea, AsideArea, CorpArea };
+const AsideArea = styled.div`
+  padding: 2rem 1.5rem 2rem 1.5rem;
+  border-bottom: 1px solid ${(props) => props.theme.colors.border.divider};
+`;
+
+const PartnersContainer = styled.div`
+  display: flex;
+
+  h3 {
+    min-width: 11rem;
+    font-size: 1.3rem;
+  }
+
+  a {
+    font-size: 1.3rem;
+    color: #000;
+
+    :not(:nth-of-type(1)) {
+      ::before {
+        content: '|';
+        margin: 0 2rem;
+        color: #d3d5d7;
+      }
+    }
+
+    :hover {
+      text-decoration-line: underline;
+    }
+  }
+`;
+const DeveloperContainer = styled.div`
+  display: flex;
+  margin-top: 1rem;
+
+  h3 {
+    min-width: 11rem;
+    font-size: 1.3rem;
+  }
+
+  a {
+    font-size: 1.3rem;
+    color: #000;
+
+    :not(:nth-of-type(1)) {
+      ::before {
+        content: '|';
+        margin: 0 2rem;
+        color: #d3d5d7;
+      }
+    }
+
+    :hover {
+      text-decoration-line: underline;
+    }
+  }
+`;
+
+const CorpArea = styled.div`
+  width: fit-content;
+  padding: 3rem 1.5rem 3rem 1.5rem;
+  margin: 0 auto;
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    font-size: 1.3rem;
+
+    li {
+      min-width: 33.3%;
+      text-align: center;
+      margin-top: 0.5rem;
+      margin-bottom: 0.5rem;
+
+      :nth-last-of-type(1) {
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+        font-weight: 600;
+        width: 100%;
+      }
+    }
+
+    a {
+      font-size: 1.3rem;
+      color: #000;
+
+      :hover {
+        text-decoration-line: underline;
+      }
+    }
+
+    ${mediaQueryLaptop} {
+      li {
+        min-width: initial;
+
+        :not(:nth-of-type(1)) {
+          ::before {
+            content: '|';
+            margin: 0 2rem;
+            color: #d3d5d7;
+          }
+        }
+
+        :nth-last-of-type(1) {
+          margin-top: 0.5rem;
+          margin-bottom: 0.5rem;
+          width: initial;
+          font-weight: 600;
+        }
+      }
+    }
+  }
+`;
+
+export {
+  Container,
+  NoticeArea,
+  AsideArea,
+  PartnersContainer,
+  DeveloperContainer,
+  CorpArea,
+};
