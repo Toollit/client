0d42097fd7ad4189e90945a4dba156c9f8683389
seungwonleee <seungwon.code@gmail.com>
2023-10-21 04:15:43 +0900

    Style: profile info design code refactoring

---
+++ models/profile/section/ProfileInfoView.tsx
@@ -0,0 +1,217 @@
+import React from 'react';
+import { PersonIcon, MailIcon } from '@/assets/icons';
+import Hashtag from '@/components/commons/hashtag';
+import { EditButton } from '@/components/commons/button';
+import { MyProfile, UserProfile } from '@/apis/profileInfoFetcher';
+import {
+  BoxContent,
+  ContentContainer,
+  Content,
+  InfoCategory,
+  Info,
+  IntroduceContentContainer,
+  ProgramSkillContainer,
+  HashtagContainer,
+} from './ProfileInfoViewStyles';
+import { BoxContainer, BoxTitle } from './styles';
+
+interface CustomMyProfile extends Omit<MyProfile, 'skills'> {
+  skills: string[];
+}
+
+interface CustomUserProfile extends Omit<UserProfile, 'skills'> {
+  skills: string[];
+}
+
+export type ProfileInfoData = CustomMyProfile | CustomUserProfile;
+
+interface ProfileInfoViewProps {
+  me: boolean;
+  data?: ProfileInfoData | null;
+  editBtnHandler: (category: string) => void;
+}
+
+const ProfileInfoView = ({
+  me,
+  data,
+  editBtnHandler,
+}: ProfileInfoViewProps) => {
+  return (
+    <>
+      {/* Profile info box */}
+      <BoxContainer>
+        <BoxTitle>내 프로필</BoxTitle>
+        <BoxContent>
+          <ContentContainer>
+            <Content>
+              <PersonIcon />
+              <Info>{data?.nickname}</Info>
+            </Content>
+
+            {me && (
+              <EditButton
+                text={'닉네임수정'}
+                category={'nickname'}
+                onClick={editBtnHandler}
+              />
+            )}
+          </ContentContainer>
+
+          {me && (
+            <ContentContainer>
+              <Content>
+                <MailIcon />
+                <Info>{data && 'email' in data && data?.email}</Info>
+              </Content>
+            </ContentContainer>
+          )}
+
+          {me && (
+            <ContentContainer>
+              <Content>
+                <InfoCategory>가입방법:</InfoCategory>
+                <Info>{data && 'signUpType' in data && data?.signUpType}</Info>
+              </Content>
+            </ContentContainer>
+          )}
+
+          <ContentContainer>
+            <Content>
+              <InfoCategory>가입일:</InfoCategory>
+              <Info>{data?.createdAt}</Info>
+            </Content>
+          </ContentContainer>
+
+          <ContentContainer>
+            <Content>
+              <InfoCategory>최종접속일:</InfoCategory>
+              <Info>{data?.lastLoginAt}</Info>
+            </Content>
+          </ContentContainer>
+        </BoxContent>
+      </BoxContainer>
+
+      {/* Introduce box */}
+      <BoxContainer>
+        <BoxTitle>자기소개</BoxTitle>
+        <IntroduceContentContainer>
+          <Info>{data?.introduce ?? '작성된 내용이 없습니다.'}</Info>
+
+          {me && (
+            <EditButton
+              text={'수정'}
+              category={'introduce'}
+              onClick={editBtnHandler}
+            />
+          )}
+        </IntroduceContentContainer>
+      </BoxContainer>
+
+      {/* Additional info box */}
+      <BoxContainer>
+        <BoxTitle>추가 정보</BoxTitle>
+        <BoxContent>
+          <ContentContainer>
+            <Content>
+              <InfoCategory>온/오프라인:</InfoCategory>
+              <Info>{data?.onOffline ?? '작성된 내용이 없습니다.'}</Info>
+            </Content>
+
+            {me && (
+              <EditButton
+                text={'수정'}
+                category={'onOffline'}
+                onClick={editBtnHandler}
+              />
+            )}
+          </ContentContainer>
+
+          <ContentContainer>
+            <Content>
+              <InfoCategory>모임장소:</InfoCategory>
+              <Info>{data?.place ?? '작성된 내용이 없습니다.'}</Info>
+            </Content>
+
+            {me && (
+              <EditButton
+                text={'수정'}
+                category={'place'}
+                onClick={editBtnHandler}
+              />
+            )}
+          </ContentContainer>
+          <ContentContainer>
+            <Content>
+              <InfoCategory>모임시간:</InfoCategory>
+              <Info>{data?.contactTime ?? '작성된 내용이 없습니다.'}</Info>
+            </Content>
+
+            {me && (
+              <EditButton
+                text={'수정'}
+                category={'contactTime'}
+                onClick={editBtnHandler}
+              />
+            )}
+          </ContentContainer>
+          <ContentContainer>
+            <Content>
+              <InfoCategory>관심분야:</InfoCategory>
+              <Info>{data?.interests ?? '작성된 내용이 없습니다.'}</Info>
+            </Content>
+
+            {me && (
+              <EditButton
+                text={'수정'}
+                category={'interests'}
+                onClick={editBtnHandler}
+              />
+            )}
+          </ContentContainer>
+          <ContentContainer>
+            <Content>
+              <InfoCategory>직무/경력:</InfoCategory>
+              <Info>{data?.career ?? '작성된 내용이 없습니다.'}</Info>
+            </Content>
+
+            {me && (
+              <EditButton
+                text={'수정'}
+                category={'career'}
+                onClick={editBtnHandler}
+              />
+            )}
+          </ContentContainer>
+        </BoxContent>
+      </BoxContainer>
+
+      {/* Usage program or skills */}
+      <BoxContainer>
+        <BoxTitle>사용 프로그램 또는 기술</BoxTitle>
+        <ProgramSkillContainer>
+          <HashtagContainer>
+            {data?.skills === null || data?.skills.length === 0
+              ? '작성된 내용이 없습니다.'
+              : data?.skills.map((hashtag, index) => {
+                  return (
+                    <Hashtag tagName={hashtag} key={`${hashtag}-${index}`} />
+                  );
+                })}
+          </HashtagContainer>
+
+          {me && (
+            <EditButton
+              text={'수정'}
+              category={'skills'}
+              onClick={editBtnHandler}
+            />
+          )}
+        </ProgramSkillContainer>
+      </BoxContainer>
+
+      {/* <ProfileFooterLink></ProfileFooterLink> */}
+    </>
+  );
+};
+
+export default ProfileInfoView;
---
+++ models/profile/section/ProfileInfoViewStyles.ts
@@ -0,0 +1,67 @@
+import styled from '@emotion/styled';
+
+const BoxContent = styled.ul`
+  li:nth-last-of-type(1) {
+    border-bottom: none;
+  }
+`;
+
+const ContentContainer = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 1rem;
+  border-bottom: 1px solid ${(props) => props.theme.colors.border.divider};
+`;
+
+const InfoCategory = styled.span`
+  font-size: 1.4rem;
+  word-break: keep-all;
+`;
+
+const Info = styled.span`
+  font-size: 1.4rem;
+  word-break: break-all;
+  padding: 0 0.5rem;
+`;
+
+const Content = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const IntroduceContentContainer = styled.li`
+  min-height: 8rem;
+  padding: 1rem 1rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProgramSkillContainer = styled.div`
+  min-height: 8rem;
+  padding: 1rem 1rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HashtagContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 1.4rem;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export {
+  BoxContent,
+  ContentContainer,
+  InfoCategory,
+  Info,
+  Content,
+  IntroduceContentContainer,
+  ProgramSkillContainer,
+  HashtagContainer,
+};
