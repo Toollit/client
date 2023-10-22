111ce4b6c1ded6b4123c3584842e7b061075eecd
seungwonleee <seungwon.code@gmail.com>
2023-10-22 23:51:50 +0900

    Add: profile page bookmark view

---
+++ models/profile/section/BookmarkView.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import { Project } from '@/apis/profileBookmarksFetcher';
+import { BoxContainer, BoxTitle } from './styles';
+import Link from 'next/link';
+import Hashtag from '@/components/commons/hashtag';
+import {
+  BoxContent,
+  Content,
+  HashtagContainer,
+  RecruitmentType,
+  RecruitmentTypeContainer,
+  StyledContentLink,
+  Title,
+  SubInfo,
+  LoadMoreContainer,
+  LoadMoreButton,
+  Notice,
+} from './BookmarkViewStyles';
+import { BookmarkIcon, PersonIcon, ViewIcon } from '@/assets/icons';
+
+interface CustomBookmark extends Omit<Project, 'memberTypes'> {
+  memberTypes: ('Developer' | 'Designer' | 'PM' | 'Anyone')[];
+}
+
+export interface BookmarkViewData {
+  bookmarks: CustomBookmark[] | null;
+  total: number;
+  showLoadMore: boolean;
+}
+
+interface BookmarkViewProps {
+  data: BookmarkViewData | null;
+  loadMore: () => void;
+}
+
+const BookmarkView = ({ data, loadMore }: BookmarkViewProps) => {
+  return (
+    <BoxContainer>
+      <BoxTitle>북마크</BoxTitle>
+      <BoxContent>
+        {data && data.total > 0 ? (
+          <>
+            {data.bookmarks?.map((project, index) => {
+              return (
+                <Content key={`/profile/project/${project.id}`}>
+                  <Link href={`/project/${project.id}`} passHref>
+                    <StyledContentLink>
+                      <RecruitmentTypeContainer>
+                        {project.memberTypes.map((type) => {
+                          return (
+                            <RecruitmentType key={type} type={type}>
+                              {type}
+                            </RecruitmentType>
+                          );
+                        })}
+                      </RecruitmentTypeContainer>
+
+                      <Title>{project.title}</Title>
+
+                      <HashtagContainer>
+                        {project.hashtags.map((hashtag) => {
+                          return (
+                            <Hashtag key={`${hashtag}`} tagName={hashtag} />
+                          );
+                        })}
+                      </HashtagContainer>
+
+                      <SubInfo>
+                        <div>
+                          <ViewIcon width={2.5} height={2.5} />
+                          <span>{project.views}</span>
+                        </div>
+                        <div>
+                          <BookmarkIcon width={2.5} height={2.5} fill={true} />
+                          <span>{project.bookmarks}</span>
+                        </div>
+                        <div>
+                          <PersonIcon width={2.5} height={2.5} />
+                          <span>{`${project.memberNumber} / ${project.recruitNumber}`}</span>
+                        </div>
+                      </SubInfo>
+                    </StyledContentLink>
+                  </Link>
+                </Content>
+              );
+            })}
+
+            {data.showLoadMore && (
+              <LoadMoreContainer>
+                <LoadMoreButton onClick={loadMore}>더보기</LoadMoreButton>
+              </LoadMoreContainer>
+            )}
+          </>
+        ) : (
+          <>
+            <Notice>북마크한 프로젝트가 없습니다.</Notice>
+          </>
+        )}
+      </BoxContent>
+    </BoxContainer>
+  );
+};
+
+export default BookmarkView;
---
+++ models/profile/section/BookmarkViewStyles.ts
@@ -0,0 +1,155 @@
+import { mediaQueryLaptop, mediaQueryTablet } from '@/styles/mediaQuery';
+import styled from '@emotion/styled';
+
+const BoxContent = styled.ul``;
+
+const Content = styled.li`
+  :hover {
+    background-color: #f0f0f0;
+  }
+
+  ::after {
+    content: '';
+    display: block;
+    padding-top: 1rem;
+    width: calc(100% - 1rem);
+    border-bottom: 1px solid ${(props) => props.theme.colors.border.divider};
+    margin: 0 auto;
+  }
+`;
+
+const StyledContentLink = styled.a`
+  display: block;
+  text-decoration: none;
+  color: #000;
+  padding: 0.5rem;
+`;
+
+const RecruitmentTypeContainer = styled.div`
+  margin: 0.6rem 0.4rem;
+  display: flex;
+  overflow: scroll;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const RecruitmentType = styled.div<{
+  type: 'Developer' | 'Designer' | 'PM' | 'Anyone';
+}>`
+  width: fit-content;
+  margin-right: 0.2rem;
+  padding: 0.4rem 0.4rem;
+  border-radius: ${(props) => props.theme.borderRadius.sharp};
+  font-size: 1rem;
+  font-weight: 600;
+  line-height: 1.5;
+  color: #fff;
+  text-align: center;
+  background-color: ${(props) => {
+    const recruitmentType = props.type;
+
+    switch (recruitmentType) {
+      case 'Developer':
+        return props.theme.colors.developer;
+
+      case 'Designer':
+        return props.theme.colors.designer;
+
+      case 'PM':
+        return props.theme.colors.pm;
+
+      case 'Anyone':
+        return props.theme.colors.anyone;
+
+      default:
+        break;
+    }
+  }};
+
+  ${mediaQueryTablet} {
+    margin-right: 0.4rem;
+    padding: 0.4rem 0.8rem;
+  }
+
+  ${mediaQueryLaptop} {
+    padding: 0.4rem 1rem;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 1.4rem;
+  padding: 0rem 0.8rem;
+  word-wrap: break-word;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  isolation: isolate; // this attribute for overflow: hidden not working in safari Bug
+`;
+
+const HashtagContainer = styled.ul`
+  margin: 0.8rem 0.5rem;
+  display: flex;
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const SubInfo = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  div {
+    display: flex;
+    line-height: 2.5rem;
+    margin-left: 1rem;
+
+    :nth-last-of-type(1) {
+      span {
+        color: #3da571;
+      }
+    }
+
+    span {
+      font-size: 1.2rem;
+      margin-left: 0.2rem;
+      margin-right: 0.2rem;
+    }
+  }
+`;
+
+const LoadMoreContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 2rem 0rem;
+`;
+const LoadMoreButton = styled.button`
+  font-size: 1.4rem;
+  padding: 1rem 3rem;
+  border: none;
+  border-radius: ${(props) => props.theme.borderRadius.base};
+  background-color: ${(props) => props.theme.colors.lightGray};
+`;
+
+const Notice = styled.p`
+  font-size: 1.4rem;
+  padding: 4rem 2rem;
+`;
+
+export {
+  Content,
+  BoxContent,
+  HashtagContainer,
+  RecruitmentTypeContainer,
+  RecruitmentType,
+  Title,
+  LoadMoreContainer,
+  LoadMoreButton,
+  StyledContentLink,
+  Notice,
+  SubInfo,
+};
