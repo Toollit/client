bd294e7676da5591f52d176160b7f7decb8f20a0
seungwonleee <seungwon.code@gmail.com>
2023-10-31 03:04:32 +0900

    Refactor: SwipeableCircularViews
    - Simplifies code while changing to swiper because library doesn't work the way i want it to

--- components/commons/swipeableView/swipeableCircularViews/index.tsx
+++ components/commons/swipeableView/swipeableCircularViews/index.tsx
@@ -1,14 +1,11 @@
-import React, { useCallback, useEffect, useState, useRef } from 'react';
+import React from 'react';
 import SwipeableViews from 'react-swipeable-views';
-import { virtualize, SlideRenderProps } from 'react-swipeable-views-utils';
-import { mod } from 'react-swipeable-views-core';
+import { autoPlay } from 'react-swipeable-views-utils';
 
-// react-swipeable-views-utils autoPlay props error occur
-const EnhancedSwipeableViews = virtualize(SwipeableViews);
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 interface SwipeableViewProps {
   children: React.ReactNode[];
-  autoPlay?: boolean;
   interval?: number;
 }
 
@@ -17,68 +14,12 @@ interface SwipeableViewProps {
  */
 const SwipeableCircularViews = ({
   children,
-  autoPlay = false,
   interval = 4000,
 }: SwipeableViewProps) => {
-  const [viewIndex, setViewIndex] = useState(0);
-  const [autoPlayRestart, setAutoPlayRestart] = useState(0);
-
-  const slideRenderer = useCallback(
-    (params: SlideRenderProps, children: React.ReactNode[]) => {
-      const { index, key } = params;
-
-      switch (mod(index, children.length)) {
-        case 0:
-          return <div key={key}>{children[0]}</div>;
-
-        case 1:
-          return <div key={key}>{children[1]}</div>;
-
-        case 2:
-          return <div key={key}>{children[2]}</div>;
-      }
-    },
-    [],
-  );
-
-  const intervalIdRef = useRef<NodeJS.Timer>();
-
-  useEffect(() => {
-    if (autoPlay) {
-      const intervalId = setInterval(() => {
-        setViewIndex((prev) => prev + 1);
-      }, interval);
-
-      intervalIdRef.current = intervalId;
-
-      return () => {
-        clearInterval(intervalId);
-      };
-    }
-  }, [viewIndex, interval, autoPlay, autoPlayRestart]);
-
   return (
-    <EnhancedSwipeableViews
-      slideRenderer={(renderer) => slideRenderer(renderer, children)}
-      enableMouseEvents
-      index={viewIndex}
-      onChangeIndex={(index) => {
-        clearInterval(intervalIdRef.current);
-        setViewIndex(index);
-      }}
-      onMouseDown={() => {
-        clearInterval(intervalIdRef.current);
-      }}
-      onMouseUp={() => {
-        setAutoPlayRestart((prev) => prev + 1);
-      }}
-      onTouchStart={() => {
-        clearInterval(intervalIdRef.current);
-      }}
-      onTouchEnd={() => {
-        setAutoPlayRestart((prev) => prev + 1);
-      }}
-    />
+    <AutoPlaySwipeableViews enableMouseEvents interval={interval}>
+      {children}
+    </AutoPlaySwipeableViews>
   );
 };
 
