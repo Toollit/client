5667152b089636c7a2ee55011632f4c8a61bbabb
seungwonleee <seungwon.code@gmail.com>
2023-10-31 01:12:03 +0900

    Fix: Mac Safari swiper CSSStyleSheet Illegal constructor error
    - Apply style in a different way

--- components/commons/swiper/webComponent/container/index.tsx
+++ components/commons/swiper/webComponent/container/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import { register } from 'swiper/element/bundle';
-import { CustomSwiperStyles } from './styles';
 
 interface SwiperProps {
   children: React.ReactNode;
@@ -15,44 +14,95 @@ const Swiper = ({ children }: SwiperProps) => {
 
     const shadowRoot = swiperRef.current?.shadowRoot;
 
-    const paginationFractionContainer = document.createElement('div');
-    paginationFractionContainer.classList.add('paginationFractionContainer');
+    // First wrapping container
+    const swiperPaginationContainer = document.createElement('div');
+    swiperPaginationContainer.classList.add('swiper-pagination-container');
 
-    const paginationElement = swiperRef.current?.shadowRoot?.querySelector(
+    // Second wrapping container
+    const swiperPaginationLayoutContainer = document.createElement('div');
+    swiperPaginationLayoutContainer.classList.add(
+      'swiper-pagination-layout-container',
+    );
+
+    // #shadow-root inside .swiper-pagination-fraction selector
+    const swiperFraction = swiperRef.current?.shadowRoot?.querySelector(
       '.swiper-pagination-fraction',
     );
 
-    paginationElement?.parentNode?.insertBefore(
-      paginationFractionContainer,
-      paginationElement,
+    swiperFraction?.parentNode?.insertBefore(
+      swiperPaginationLayoutContainer,
+      swiperFraction,
     );
 
-    if (!paginationElement) {
+    swiperPaginationLayoutContainer.parentNode?.insertBefore(
+      swiperPaginationContainer,
+      swiperPaginationLayoutContainer,
+    );
+
+    if (!swiperFraction) {
       return;
     }
 
-    paginationFractionContainer.appendChild(paginationElement);
+    // Wrapped in the order below
+    // .swiper-pagination-container > .swiper-pagination-layout-container > .swiper-pagination-fraction
+    swiperPaginationLayoutContainer.appendChild(swiperFraction);
+    swiperPaginationContainer.appendChild(swiperPaginationLayoutContainer);
 
     if (!shadowRoot) {
       return;
     }
 
+    /* Mac Safari swiper error - CSSStyleSheet Unhandled Runtime Error TypeError: Illegal constructor */
+    /*
     const sheet = new CSSStyleSheet();
-
     sheet.replaceSync(`
-    .paginationFractionContainer { position:relative; max-width:102.4rem; width:100%; margin:0 auto}
+    .swiperPaginationLayoutContainer { position:relative; max-width:102.4rem; width:100%; margin:0 auto}
     .swiper-pagination-fraction {font-size:1.8rem; background: rgba(0, 0, 0, 0.2); color:#fff; width:fit-content; height:3rem; border-radius:25rem; margin-left:1rem; }
     .swiper-pagination-current {padding-left:2rem}
     .swiper-pagination-total {padding-right:2rem}
-    `);
-
+    `); 
     shadowRoot.adoptedStyleSheets.push(sheet);
+    */
+
+    const style = document.createElement('style');
+    style.innerHTML = `
+    .swiper-pagination-container { 
+      position:relative;
+      max-width:102.4rem; 
+      width:100%; 
+      margin:0 auto; 
+    }
+
+    .swiper-pagination-layout-container { 
+      position:absolute; 
+      right:0; 
+      width: 8rem;
+    }
+
+    .swiper-pagination-fraction {
+      font-size:1.4rem; 
+      background: rgba(0, 0, 0, 0.2); 
+      color:#fff !important; 
+      width:fit-content !important; 
+      height:2.4rem; 
+      border-radius:25rem;
+    }
+    
+    .swiper-pagination-current {
+      padding-left:1.5rem
+    }
+
+    .swiper-pagination-total {
+      padding-right:1.5rem
+    }
+    `;
+
+    shadowRoot.appendChild(style);
   }, []);
 
   return (
     <>
       <swiper-container
-        className='swiperContainer'
         ref={swiperRef}
         autoplay={true}
         effect='fade'
@@ -61,7 +111,6 @@ const Swiper = ({ children }: SwiperProps) => {
       >
         {children}
       </swiper-container>
-      <CustomSwiperStyles />
     </>
   );
 };
