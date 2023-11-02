147a145dd0dc90071a9288758252abf958bddb4a
seungwonleee <seungwon.code@gmail.com>
2023-11-02 21:49:23 +0900

    Refactor: profile bookmark tab seperate skeleton and content container

--- models/profile/tab/bookmark/BookmarkView.tsx
+++ models/profile/tab/bookmark/BookmarkView.tsx
@@ -36,75 +36,76 @@ export interface BookmarkViewProps {
 
 const BookmarkView = ({ data, loadMore }: BookmarkViewProps) => {
   return (
-    <BoxContainer>
-      <BoxTitle>북마크</BoxTitle>
+    <>
       {data && (
-        <BoxContent>
-          {data.total > 0 ? (
-            <>
-              {data.bookmarks?.map((project, index) => {
-                return (
-                  <Content key={`/profile/project/${project.id}`}>
-                    <Link href={`/project/${project.id}`} passHref>
-                      <StyledContentLink>
-                        <RecruitmentTypeContainer>
-                          {project.memberTypes.map((type) => {
-                            return (
-                              <RecruitmentType key={type} type={type}>
-                                {type}
-                              </RecruitmentType>
-                            );
-                          })}
-                        </RecruitmentTypeContainer>
+        <BoxContainer>
+          <BoxTitle>북마크</BoxTitle>
+          <BoxContent>
+            {data.total > 0 ? (
+              <>
+                {data.bookmarks?.map((project, index) => {
+                  return (
+                    <Content key={`/profile/project/${project.id}`}>
+                      <Link href={`/project/${project.id}`} passHref>
+                        <StyledContentLink>
+                          <RecruitmentTypeContainer>
+                            {project.memberTypes.map((type) => {
+                              return (
+                                <RecruitmentType key={type} type={type}>
+                                  {type}
+                                </RecruitmentType>
+                              );
+                            })}
+                          </RecruitmentTypeContainer>
 
-                        <Title>{project.title}</Title>
+                          <Title>{project.title}</Title>
 
-                        <HashtagContainer>
-                          {project.hashtags.map((hashtag) => {
-                            return (
-                              <Hashtag key={`${hashtag}`} tagName={hashtag} />
-                            );
-                          })}
-                        </HashtagContainer>
+                          <HashtagContainer>
+                            {project.hashtags.map((hashtag) => {
+                              return (
+                                <Hashtag key={`${hashtag}`} tagName={hashtag} />
+                              );
+                            })}
+                          </HashtagContainer>
 
-                        <SubInfo>
-                          <div>
-                            <ViewIcon width={2.5} height={2.5} />
-                            <span>{project.views}</span>
-                          </div>
-                          <div>
-                            <BookmarkIcon
-                              width={2.5}
-                              height={2.5}
-                              fill={true}
-                            />
-                            <span>{project.bookmarkCount}</span>
-                          </div>
-                          <div>
-                            <PersonIcon width={2.5} height={2.5} />
-                            <span>{`${project.memberCount} / ${project.recruitCount}`}</span>
-                          </div>
-                        </SubInfo>
-                      </StyledContentLink>
-                    </Link>
-                  </Content>
-                );
-              })}
+                          <SubInfo>
+                            <div>
+                              <ViewIcon width={2.5} height={2.5} />
+                              <span>{project.views}</span>
+                            </div>
+                            <div>
+                              <BookmarkIcon
+                                width={2.5}
+                                height={2.5}
+                                fill={true}
+                              />
+                              <span>{project.bookmarkCount}</span>
+                            </div>
+                            <div>
+                              <PersonIcon width={2.5} height={2.5} />
+                              <span>{`${project.memberCount} / ${project.recruitCount}`}</span>
+                            </div>
+                          </SubInfo>
+                        </StyledContentLink>
+                      </Link>
+                    </Content>
+                  );
+                })}
 
-              {data.showLoadMore && (
-                <LoadMoreContainer>
-                  <LoadMoreButton onClick={loadMore}>더보기</LoadMoreButton>
-                </LoadMoreContainer>
-              )}
-            </>
-          ) : (
-            <>
-              <Notice>북마크한 프로젝트가 없습니다.</Notice>
-            </>
-          )}
-        </BoxContent>
+                {data.showLoadMore && (
+                  <LoadMoreContainer>
+                    <LoadMoreButton onClick={loadMore}>더보기</LoadMoreButton>
+                  </LoadMoreContainer>
+                )}
+              </>
+            ) : (
+              <>
+                <Notice>북마크한 프로젝트가 없습니다.</Notice>
+              </>
+            )}
+          </BoxContent>
+        </BoxContainer>
       )}
-
       {!data && (
         <>
           <Skeleton height={20} top={3} />
@@ -113,7 +114,7 @@ const BookmarkView = ({ data, loadMore }: BookmarkViewProps) => {
           <Skeleton height={20} top={3} />
         </>
       )}
-    </BoxContainer>
+    </>
   );
 };
 
