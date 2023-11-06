a7790b9ce63cc25d48d5749a0b053a57c30f7da4
seungwonleee <seungwon.code@gmail.com>
2023-11-06 23:11:36 +0900

    Add: margin padding common styles

--- styles/commons.ts
+++ styles/commons.ts
@@ -1,4 +1,27 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+export interface MarginPaddingProps {
+  marginTop?: `${number}rem`;
+  marginRight?: `${number}rem`;
+  marginBottom?: `${number}rem`;
+  marginLeft?: `${number}rem`;
+  paddingTop?: `${number}rem`;
+  paddingRight?: `${number}rem`;
+  paddingBottom?: `${number}rem`;
+  paddingLeft?: `${number}rem`;
+}
+
+const commonMarginPaddingStyles = (props: MarginPaddingProps) => css`
+  margin-top: ${props.marginTop ? props.marginTop : 0};
+  margin-right: ${props.marginRight ? props.marginRight : 0};
+  margin-bottom: ${props.marginBottom ? props.marginBottom : 0};
+  margin-left: ${props.marginLeft ? props.marginLeft : 0};
+  padding-top: ${props.paddingTop ? props.paddingTop : 0};
+  padding-right: ${props.paddingRight ? props.paddingRight : 0};
+  padding-bottom: ${props.paddingBottom ? props.paddingBottom : 0};
+  padding-left: ${props.paddingLeft ? props.paddingLeft : 0};
+`;
 
 interface SVGContainerProps {
   width: number;
@@ -31,12 +54,12 @@ const InnerContainer = styled.div`
   width: 100%;
 `;
 
-const BoxContainer = styled.div`
+const BoxContainer = styled.div<MarginPaddingProps>`
   border: 1px solid ${(props) => props.theme.colors.border.base};
   border-radius: ${(props) => props.theme.borderRadius.base};
   border-top: none;
   box-shadow: ${(props) => props.theme.boxShadow.base};
-  margin-top: 3rem;
+  ${commonMarginPaddingStyles}
 `;
 
 const BoxTitle = styled.div`
