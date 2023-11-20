58f2a698bd9ff251c22bb38ebf717a8e6c353ab1
seungwonleee <seungwon.code@gmail.com>
2023-11-20 23:26:44 +0900

    Update: useWindowSize hooks change default state
    - isMobile, isTablet, isLaptop default value false interfere with updating the screen size and content rendering properly.

--- hooks/useWindowSize.ts
+++ hooks/useWindowSize.ts
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
+interface WindowSizeState {
+  width: number;
+  height: number;
+  isMobile: boolean | null;
+  isTablet: boolean | null;
+  isLaptop: boolean | null;
+}
+
 const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState({
+  const [windowSize, setWindowSize] = useState<WindowSizeState>({
     width: 0,
     height: 0,
-    isMobile: false,
-    isTablet: false,
-    isLaptop: false,
+    isMobile: null,
+    isTablet: null,
+    isLaptop: null,
   });
+
   useEffect(() => {
     const handleResize = () => {
       setWindowSize({
