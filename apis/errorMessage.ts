747fbd91f0b1cb29d19e5d995a3c89c514ff88ee
seungwonleee <seungwon.code@gmail.com>
2023-12-03 23:13:20 +0900

    Update: error message handler mutate data when status code is 409

--- apis/errorMessage.ts
+++ apis/errorMessage.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import { AxiosErrorData } from '@/apis/types';
+import { KeyedMutator } from 'swr';
 
-export const errorMessage = (error: unknown) => {
+export const errorMessage = <T>(error: unknown, mutate?: KeyedMutator<T>) => {
   if (axios.isAxiosError<AxiosErrorData>(error)) {
+    if ((error.response?.status === 409, mutate)) {
+      mutate();
+    }
+
     return alert(error.response?.data.message);
   } else {
     return alert('죄송합니다. 오류가 발생했습니다. 나중에 다시 시도해주세요.');
