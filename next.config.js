40bbdf2159c682fc6a03df4e30b9418efdb480b3
seungwonleee <seungwon.code@gmail.com>
2024-02-26 07:21:13 +0900

    Fix: next.config.js apply webpack resolve config for import error with '@' alias
    - reference: https://nextjs.org/docs/messages/invalid-resolve-alias
    - reference: https://stackoverflow.com/questions/76676456/next-js-v13-in-docker-does-not-respect-path-alias-but-works-locally

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const s3Url = process.env.NEXT_PUBLIC_S3_URL;
 const isDev = process.env.NODE_ENV !== 'production';
 
@@ -9,6 +10,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   reactStrictMode: false,
   webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, '/'),
+    };
     return {
       ...config,
       mode: isDev ? 'development' : 'production',
