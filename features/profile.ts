b103449a3508c9fc1be47d3ca1a2e22169399e78
seungwonleee <seungwon.code@gmail.com>
2024-06-25 20:24:44 +0900

    Update: profile redux store delete unused state

--- features/profile.ts
+++ features/profile.ts
@@ -2,14 +2,10 @@ import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
 interface InitialState {
-  isRegisteredUser: boolean;
-  userNickname: string;
   tab: 'viewProfile' | 'viewProjects' | 'viewBookmarks' | 'viewNotifications';
 }
 
 const initialState: InitialState = {
-  isRegisteredUser: false,
-  userNickname: '',
   tab: 'viewProfile',
 };
 
@@ -17,20 +13,6 @@ const profileSlice = createSlice({
   name: 'profile',
   initialState,
   reducers: {
-    updateProfileRegisteredUserStatus: (
-      state,
-      action: PayloadAction<Pick<InitialState, 'isRegisteredUser'>>,
-    ) => {
-      const { isRegisteredUser } = action.payload;
-      state.isRegisteredUser = isRegisteredUser;
-    },
-    updateProfileUserNickname: (
-      state,
-      action: PayloadAction<Pick<InitialState, 'userNickname'>>,
-    ) => {
-      const { userNickname } = action.payload;
-      state.userNickname = userNickname;
-    },
     updateProfileTab: (
       state,
       action: PayloadAction<Pick<InitialState, 'tab'>>,
@@ -42,10 +24,6 @@ const profileSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const {
-  updateProfileRegisteredUserStatus,
-  updateProfileUserNickname,
-  updateProfileTab,
-} = profileSlice.actions;
+export const { updateProfileTab } = profileSlice.actions;
 
 export default profileSlice.reducer;
