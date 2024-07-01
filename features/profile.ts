5346d98a24d5ebdc133c9c608bb779d5374c2b49
seungwonleee <seungwon.code@gmail.com>
2024-07-01 23:19:52 +0900

    Add: profile redux store user nickname and page render check state

--- features/profile.ts
+++ features/profile.ts
@@ -3,10 +3,20 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 
 interface InitialState {
   tab: 'viewProfile' | 'viewProjects' | 'viewBookmarks' | 'viewNotifications';
+  userNickname: string;
+  hasRenderedViewProfile: boolean;
+  hasRenderedViewProjects: boolean;
+  hasRenderedViewBookmarks: boolean;
+  hasRenderedViewNotifications: boolean;
 }
 
 const initialState: InitialState = {
   tab: 'viewProfile',
+  userNickname: '',
+  hasRenderedViewProfile: false,
+  hasRenderedViewProjects: false,
+  hasRenderedViewBookmarks: false,
+  hasRenderedViewNotifications: false,
 };
 
 const profileSlice = createSlice({
@@ -19,11 +29,34 @@ const profileSlice = createSlice({
     ) => {
       const { tab } = action.payload;
       state.tab = tab;
+      switch (tab) {
+        case 'viewProfile':
+          state.hasRenderedViewProfile = true;
+          break;
+        case 'viewProjects':
+          state.hasRenderedViewProjects = true;
+          break;
+        case 'viewBookmarks':
+          state.hasRenderedViewBookmarks = true;
+          break;
+        case 'viewNotifications':
+          state.hasRenderedViewNotifications = true;
+          break;
+        default:
+          break;
+      }
+    },
+    updateProfileNickname: (
+      state,
+      action: PayloadAction<Pick<InitialState, 'userNickname'>>,
+    ) => {
+      const { userNickname } = action.payload;
+      state.userNickname = userNickname;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { updateProfileTab } = profileSlice.actions;
+export const { updateProfileTab, updateProfileNickname } = profileSlice.actions;
 
 export default profileSlice.reducer;
