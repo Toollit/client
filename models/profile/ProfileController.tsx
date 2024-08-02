bf0b21289e6856846410a589bc5b35e682d2d27a
seungwonleee <seungwon.code@gmail.com>
2024-08-02 16:37:32 +0900

    Fix: profile main controller modify unnecessarily cached data erasure logic

--- models/profile/ProfileController.tsx
+++ models/profile/ProfileController.tsx
@@ -1,19 +1,12 @@
 import React, { FC, useCallback, useEffect, useRef } from 'react';
 import ProfileView, { ViewProps } from './ProfileView';
-import useSWR, { Cache } from 'swr';
 import { useRouter } from 'next/router';
-import type { ScopedMutator } from 'swr/_internal';
 import useAuth from '@/hooks/useAuth';
 import useLogout from '@/hooks/useLogout';
 import useCachedKeys from '@/hooks/useCachedKeys';
 import { useAppDispatch, useAppSelector } from '@/store';
 import { updateProfileTab, updateProfileNickname } from '@/features/profile';
 import useUserRegisteredCheckSWR from '@/hooks/useSWR/useUserRegisteredCheckSWR';
-
-interface CachedData<T> {
-  cache: Cache<T | undefined>;
-  mutate: ScopedMutator;
-}
 
 export type ProfileTab =
   | 'viewProfile'
@@ -28,7 +21,7 @@ const ProfileController: FC<ControllerProps> = () => {
   const dispatch = useAppDispatch();
   const { logout } = useLogout();
   const { user } = useAuth();
-  const { clearCache } = useCachedKeys();
+  const { mutatePage } = useCachedKeys();
 
   const tab = useAppSelector((state) => state.profile.tab);
   const userNickname = useAppSelector((state) => state.profile.userNickname);
@@ -56,10 +49,9 @@ const ProfileController: FC<ControllerProps> = () => {
 
   useEffect(() => {
     return () => {
-      // TODO 이부분 꼭 필요한지 확인하고 필요없으면 삭제하고 필요하면 북마크 오류 어떻게 해결할지 확인
-      // clearCache();
+      mutatePage({ page: '/profile' });
     };
-  }, [clearCache, dispatch]);
+  }, [dispatch, mutatePage]);
 
   // useEffect for troubleshooting nickname undefined upon reload or tab movement
   useEffect(() => {
